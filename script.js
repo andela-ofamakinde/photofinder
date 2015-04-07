59d1019f4b9f450a0a78f81c189165067755b4ce
andela-ofamakinde <olatoyosi.famakinde@andela.co>
2015-04-07 12:34:36 +0100

    API key updated

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
    
-   $('#submit').click(function(){
-   	var tag = $('#tag').val();
+ $('#submit').click(function(){
+ 	var tag = $('#tag').val();
 
-   	$.ajax({
-   		url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=15410c054b26fa22e0278738aea213bb&tags=" + tag + "&per_page=20&format=json&nojsoncallback=1",
+ 	$.ajax({
+ 		url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f3b29e6157ba3c709b16e46755ab83ad&tags=" + tag + "&per_page=20&format=json&nojsoncallback=1",
+ 		success: function(data) {
+ 			$('#photos').empty();
+ 			console.log(data);
 
-   		success: function(data) {
-   			$('#photos').empty();
-   			var photoArray = data.photos.photo;
+ 		var photoArray = data.photos.photo;
 
-   			$.each(photoArray, function(index, value) {
-   				var farmId = value.farm;
-   				var serverId = value.server;
-   				var photoId = value.id;
-   				var secret = value.secret;
+ 			$.each(photoArray, function(index, value) {
+ 				var farmId = value.farm;
+ 				var serverId = value.server;
+ 				var photoId = value.id;
+ 				var secret = value.secret;
 
-   				var imgSrc = "https://farm"+farmId+".staticflickr.com/"+ serverId+ "/"+photoId+"_"+secret+ "_m.jpg"
+ 				var imgSrc = "https://farm"+farmId+".staticflickr.com/"+ serverId+ "/"+photoId+"_"+secret+ "_m.jpg"
 
-   				$('#photos').append("<img id='searchImg' src=" + imgSrc + ">");
+ 				$('#photos').append("<img id='searchImg' src=" + imgSrc + ">");
 
-   			});
-   		}
+ 			});
+ 		}
    	});
    	
    });
